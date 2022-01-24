4b3b428da71f57e13fd27fb75494f9f7a78f204f
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-01-24 14:31:40 +0530

    CN_Hash

--- profile.py
+++ profile.py
@@ -139,7 +139,7 @@ LOWLAT_IMG = "urn:publicid:IDN+emulab.net+image+PowderTeam:U18LL-SRSLTE"
 UBUNTU_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD"
 COMP_MANAGER_ID = "urn:publicid:IDN+emulab.net+authority+cm"
 DEFAULT_NR_RAN_HASH = "8082394371e5abcec8a7ab4cf501d79df6acd3e5"
-DEFAULT_NR_CN_HASH = "v2021-12-14-01"
+DEFAULT_NR_CN_HASH = "ea0af60d17ad92302a127095d92d8f54f432d6df"
 OAI_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-oai.sh")
 
 BENCH_SDR_IDS = {
