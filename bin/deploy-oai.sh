0f264bff8babb2b35e1d2efcb5cfb15d37e3074c
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-02-02 22:11:01 +0530

    ULCL repository changed

--- bin/deploy-oai.sh
+++ bin/deploy-oai.sh
@@ -6,7 +6,7 @@ SRCDIR=/var/tmp
 CFGDIR=/local/repository/etc
 OAI_RAN_MIRROR="https://gitlab.flux.utah.edu/powder-mirror/openairinterface5g"
 OAI_CN5G_REPO="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed"
-CN5G_REPO="https://github.com/pragnyakiri/free5gc-compose"
+CN5G_REPO="https://github.com/pragnyakiri/free5gc-ULCL-compose"
 SRS_REPO="https://github.com/srsran/srsRAN"
 
 if [ -f $SRCDIR/oai-setup-complete ]; then
--- profile.py
+++ profile.py
@@ -141,7 +141,7 @@ UBUNTU1_IMG = "urn:publicid:IDN+emulab.net+image+reu2020:ubuntu1864std50023gener
 COMP_MANAGER_ID = "urn:publicid:IDN+emulab.net+authority+cm"
 DEFAULT_NR_RAN_HASH = "8082394371e5abcec8a7ab4cf501d79df6acd3e5"
 #DEFAULT_NR_CN_HASH = "1d0ab4b54b3a0ab247d1a2d140b44b3ddd155fa8"
-DEFAULT_NR_CN_HASH = "6ff269ff51b7226e7cb097383f6662365bde4444"
+DEFAULT_NR_CN_HASH = "2155205fcc16edde17483748388b3d59853c6187"
 OAI_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-oai.sh")
 
 BENCH_SDR_IDS = {
