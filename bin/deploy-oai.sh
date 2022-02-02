8f44675f1ef92fd5a102333b617d0d738b0fd2b8
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-02-02 12:02:47 +0530

    gtp5g 1

--- bin/deploy-oai.sh
+++ bin/deploy-oai.sh
@@ -67,7 +67,7 @@ function setup_cn_node {
     sudo chmod +x /usr/local/bin/docker-compose
 
     #Install gtp5g for upf to work
-    git clone https://github.com/PrinzOwO/gtp5g.git
+    git clone https://github.com/PrinzOwO/gtp5g.git gtp5g
     cd gtp5g
     make
     sudo make install
